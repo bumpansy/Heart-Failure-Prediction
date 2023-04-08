6d5d3c337e10b05a41cb6ba553852743a61c817a
bumpansy <suyogkasture75@gmail.com>
2023-04-08 14:46:25 +0530

    EDA py

--- App/App.py
+++ App/App.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pickle
 import pandas as pd
+from EDA.EDA import *
+
+st.set_page_config(layout='wide')
 
 url = 'https://raw.githubusercontent.com/bumpansy/Heart-Failure-Prediction/main/Model/heart.csv'
 df = pd.read_csv(url,index_col=1)
@@ -17,3 +20,6 @@ with tab1:
     st.write('Number of Rows: 303')
     st.write('Number of Columns: 14')
     st.dataframe(df)
+    col1,col2 = st.columns()
+    with col1:
+        st.plotly_chart(sex_hist)
