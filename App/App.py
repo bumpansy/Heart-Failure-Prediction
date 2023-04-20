7b5b96361aeb249ef2dc9da2c6e0f2c686ae95b6
bumpansy <suyogkasture75@gmail.com>
2023-04-20 11:54:30 +0530

    tf model

--- App/App.py
+++ App/App.py
@@ -4,12 +4,21 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 #from EDA.EDA import *
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+
+#model = load_model('E:\Public Repositories\Heart-Failure-Prediction\Model\my_model.h5')
+
+model = tf.keras.saving.load_model(
+    'E:\Public Repositories\Heart-Failure-Prediction\Model\my_model.tf', custom_objects=None, compile=True, safe_mode=True
+)
 
 st.set_page_config(layout='wide')
 
 url = 'https://raw.githubusercontent.com/bumpansy/Heart-Failure-Prediction/main/Model/heart.csv'
-df = pd.read_csv('E:\Public Repositories\Heart-Failure-Prediction\Model\heart.csv')
-model = pickle.load(open('https://raw.githubusercontent.com/bumpansy/Heart-Failure-Prediction/blob/7bd6923a29a0b8939a168671e439b5f6523babf7/Model/model.pkl', 'rb'))
+df = pd.read_csv(url)
+
+#model = pickle.load(open('E:\\Public Repositories\\Heart-Failure-Prediction\\Model\\newmodel.pkl', 'rb'))
 
 def user_input_features():
     col1, col2 = st.columns(2)
@@ -49,13 +58,18 @@ def user_input_features():
 st.header('Heart Failure Prediction App')
 st.write('This app is made to predict the probability of heart failure in a person using his/her vital information.')
 
-tab1,tab2,tab3 = st.tabs(['Dataset', 'Model', 'Prediction'])
+tab1,tab2,tab3,tab4 = st.tabs(['Dataset', 'Model', 'Prediction', 'About Me'])
 with tab1:
-    st.subheader('About the Dataset')
-    st.write('Link: https://www.kaggle.com/datasets/fedesoriano/heart-failure-prediction')
-    st.write('Number of Rows: 303')
-    st.write('Number of Columns: 14')
-    st.dataframe(df)
+    col1, col2 = st.columns(2)
+    with col1:
+
+        st.subheader('About the Dataset')
+        st.write('Link: https://www.kaggle.com/datasets/fedesoriano/heart-failure-prediction')
+        st.write('Number of Rows: 303')
+        st.write('Number of Columns: 14')
+    with col2:
+
+        st.dataframe(df)
     col1,col2 = st.columns(2)
     with col1:
         fig = go.Figure(px.histogram(df, x = 'sex', color = 'sex'))
@@ -64,11 +78,15 @@ with tab1:
         fig = px.density_heatmap(df)
         st.plotly_chart(fig, x = '' ,use_container_width=True)
 
+with tab2:
+    st.subheader('Model Info')
+
 with tab3:
     st.subheader('Heart Failure Prediction')
     with st.form('Vitals Input'):
         st.write('Enter the correct details for the given inout fields.')
         input_df = user_input_features()
         submit = st.form_submit_button('Predict')
-        if submit:
-            st.dataframe(input_df)
+    if submit:
+        st.dataframe(input_df)
+        prediction = model.predict()
